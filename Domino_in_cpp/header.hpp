07eba02803df3a8b3d97655f4d4fb23aa8662479
Strzelacz48 <marcin@DESKTOP-FT92BPV.localdomain>
2022-06-09 05:59:50 +0200

    Projekt pliki w Cpp

---
+++ Domino_in_cpp/header.hpp
@@ -0,0 +1,40 @@
+class Menu{
+    Game1 dobierane;
+    Game2 zwykle;
+    Game3 piatkowe;
+    Game4 trojkowe;
+public:
+    void start();
+    void settings();
+    void stop();
+};
+class Game1{
+public:
+    void start();
+    void settings();
+    void stop();
+};
+class Game2{
+public:
+    void start();
+    void settings();
+    void stop();
+};
+class Game3{
+public:
+    void start();
+    void settings();
+    void stop();
+};
+class Game4{
+public:
+    void start();
+    void settings();
+    void stop();
+};
+class klocek{
+    int gora;
+    int dol;
+};
+class plansza{
+};
