22cf4f5194ba84a93466111713dd0e23a19aff1d
Strzelacz48 <marcin@DESKTOP-FT92BPV.localdomain>
2022-06-23 20:56:36 +0200

    Git wymaga nie wiadomo czego

--- Domino_in_cpp/header.hpp
+++ Domino_in_cpp/header.hpp
@@ -1,40 +1,51 @@
-class Menu{
+class Menu
+{
     Game1 dobierane;
     Game2 zwykle;
     Game3 piatkowe;
     Game4 trojkowe;
+
 public:
     void start();
     void settings();
     void stop();
 };
-class Game1{
+class Game1
+{
 public:
     void start();
     void settings();
     void stop();
 };
-class Game2{
+class Game2
+{
 public:
     void start();
     void settings();
     void stop();
 };
-class Game3{
+class Game3
+{
 public:
     void start();
     void settings();
     void stop();
 };
-class Game4{
+class Game4
+{
 public:
     void start();
     void settings();
     void stop();
 };
-class klocek{
+class klocek
+{
     int gora;
     int dol;
 };
-class plansza{
+class plansza
+{
+    klocek reka1[];
+    klocek reka2[];
+    klocek pula[];
 };
